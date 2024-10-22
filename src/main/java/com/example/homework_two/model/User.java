c45af7c12d8aa549d5c110758664e262f22aeb84
denis <denis13071981@gmail.com>
2024-10-22 23:45:49 +0300

    Homework_two

---
+++ src/main/java/com/example/homework_two/model/User.java
@@ -0,0 +1,49 @@
+package com.example.homework_two.model;
+
+public class User {
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
---
+++ src/main/java/com/example/homework_two/services/UserService.java
@@ -0,0 +1,38 @@
+package com.example.homework_two.services;
+
+import com.example.homework_two.model.User;
+import com.example.homework_two.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserById(Integer id) {
+        return userRepository.findUserById(id);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public void deleteUserById(int id) {
+        userRepository.deleteById(id);
+    }
+
+    public void updateUser(User user) {
+        userRepository.update(user);
+    }
+
+}
